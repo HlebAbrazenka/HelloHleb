afb40b88f9192793cf9102e07ac2735d153b6d8f
HlebAbrazenka <dr.bread92@gmail.com>
2022-12-28 21:18:22 +0100

    home

---
+++ src/Home.java
@@ -0,0 +1,11 @@
+/**
+ * Java. My first program
+ *
+ * @author HlebAbrazenka
+ * @version 28.12.2022
+ */
+public class Home {
+    public static void main(String[] args) {
+        System.out.print("HELLO IqT"); //output
+    }
+}
